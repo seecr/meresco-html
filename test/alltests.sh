291f3ae0abdf5b725db9b21bc8331497fdbfb786
JJ <JJ@7f82bda0-831b-0410-895c-c142f06d1e64>
2011-10-10 14:57:27 +0000

    HM/JJ: made test use default python version; updated setup.py and fixed license
    git-svn-id: svn+ssh://sources.seecr.nl/svn/dynamichtml/workingsets/0.7.2-Seecr/version_0-python2.6@17789 7f82bda0-831b-0410-895c-c142f06d1e64

--- test/alltests.sh
+++ test/alltests.sh
@@ -1,5 +1,29 @@
-#!/bin/bash
+## begin license ##
+# 
+# "DynamicHtml" is a template engine based on generators, and a sequel to Slowfoot. 
+# 
+# Copyright (C) 2011 Seecr (Seek You Too B.V.) http://seecr.nl
+# 
+# This file is part of "DynamicHtml"
+# 
+# "DynamicHtml" is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# "DynamicHtml" is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with "DynamicHtml"; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+# 
+## end license ##
+
 export LANG=en_US.UTF-8
 export PYTHONPATH=.:"$PYTHONPATH"
-python2.5 _alltests.py "$@"
+pyversion="$(pyversions --default)"
+${pyversion} _alltests.py "$@"
 
