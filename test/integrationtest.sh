57e89acf42b8f2ded8620f28edb6cad26ace0039
Thijs Janssen <thijs@seecr.nl>
2012-12-27 16:44:32 +0100

    TJ/JJ: license.

--- test/integrationtest.sh
+++ test/integrationtest.sh
@@ -1,29 +1,27 @@
 #!/bin/bash
 ## begin license ##
 # 
-# "Edurep" is a service for searching in educational repositories.
-# "Edurep" is developed for Stichting Kennisnet (http://www.kennisnet.nl) by
-# Seek You Too (http://www.cq2.nl). The project is based on the opensource
-# project Meresco (http://www.meresco.com). 
+# "Seecr Html" is a template engine based on generators, and a sequel to Slowfoot. 
+# It is also known as "DynamicHtml". 
 # 
 # Copyright (C) 2006-2011 Seek You Too (CQ2) http://www.cq2.nl
 # Copyright (C) 2006-2011 Stichting Kennisnet http://www.kennisnet.nl
-# Copyright (C) 2011 Seecr (Seek You Too B.V.) http://seecr.nl
+# Copyright (C) 2011-2012 Seecr (Seek You Too B.V.) http://seecr.nl
 # 
-# This file is part of "Edurep"
+# This file is part of "Seecr Html"
 # 
-# "Edurep" is free software; you can redistribute it and/or modify
+# "Seecr Html" is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
 # 
-# "Edurep" is distributed in the hope that it will be useful,
+# "Seecr Html" is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 # 
 # You should have received a copy of the GNU General Public License
-# along with "Edurep"; if not, write to the Free Software
+# along with "Seecr Html"; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 # 
 ## end license ##
