407f6c634777e5b25a6dc259a9cbf2e3b6d47a29
Thijs Janssen <thijs@seecr.nl>
2013-01-04 14:13:23 +0100

    TS/TJ: license 2013.

--- seecr/html/dynamichtml.py
+++ seecr/html/dynamichtml.py
@@ -1,25 +1,26 @@
 # -*- coding: utf-8 -*-
 ## begin license ##
 # 
-# "DynamicHtml" is a template engine based on generators, and a sequel to Slowfoot. 
+# "Seecr Html" is a template engine based on generators, and a sequel to Slowfoot. 
+# It is also known as "DynamicHtml". 
 # 
 # Copyright (C) 2008-2011 Seek You Too (CQ2) http://www.cq2.nl
-# Copyright (C) 2011-2012 Seecr (Seek You Too B.V.) http://seecr.nl
+# Copyright (C) 2011-2013 Seecr (Seek You Too B.V.) http://seecr.nl
 # 
-# This file is part of "DynamicHtml"
+# This file is part of "Seecr Html"
 # 
-# "DynamicHtml" is free software; you can redistribute it and/or modify
+# "Seecr Html" is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
 # 
-# "DynamicHtml" is distributed in the hope that it will be useful,
+# "Seecr Html" is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 # 
 # You should have received a copy of the GNU General Public License
-# along with "DynamicHtml"; if not, write to the Free Software
+# along with "Seecr Html"; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 # 
 ## end license ##
