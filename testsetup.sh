0bccacd4b76ebe4dcc5dce09da800d0722cb06c7
Thijs Janssen <thijs@seecr.nl>
2013-05-17 13:47:51 +0200

    TJ: testing for python2.7 on wheezy.

--- testsetup.sh
+++ testsetup.sh
@@ -1,47 +1,54 @@
 #!/bin/bash
 ## begin license ##
-# 
-# "Seecr Html" is a template engine based on generators, and a sequel to Slowfoot. 
-# It is also known as "DynamicHtml". 
-# 
+#
+# "Seecr Html" is a template engine based on generators, and a sequel to Slowfoot.
+# It is also known as "DynamicHtml".
+#
 # Copyright (C) 2008-2009 Seek You Too (CQ2) http://www.cq2.nl
-# Copyright (C) 2012 Seecr (Seek You Too B.V.) http://seecr.nl
-# 
+# Copyright (C) 2012-2013 Seecr (Seek You Too B.V.) http://seecr.nl
+#
 # This file is part of "Seecr Html"
-# 
+#
 # "Seecr Html" is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
-# 
+#
 # "Seecr Html" is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with "Seecr Html"; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-# 
+#
 ## end license ##
 
-set -e
+set -o errexit
 rm -rf tmp build
 mydir=$(cd $(dirname $0); pwd)
 source /usr/share/seecr-test/functions
 
+pyversions="2.6"
+if distro_is_debian_wheezy; then
+    pyversions="2.6 2.7"
+fi
+
 VERSION="x.y.z"
 
-definePythonVars
-
-${PYTHON} setup.py install --root tmp
-cp seecr/__init__.py ${SITEPACKAGES}/seecr/
+for pyversion in $pyversions; do
+    definePythonVars $pyversion
+    echo "###### $pyversion, $PYTHON"
+    ${PYTHON} setup.py install --root tmp
+done
 cp -r test tmp/test
-find tmp -type f -exec sed -r -e \
-    "s,^docDir.*$,docDir = '$mydir/tmp/usr/share/doc/seecr-html'," -i {} \;
-
 removeDoNotDistribute tmp
+find tmp -name '*.py' -exec sed -r -e "
+    s/\\\$Version:[^\\\$]*\\\$/\\\$Version: ${VERSION}\\\$/;
+    " -i '{}' \;
 
+cp -r test tmp/test
 runtests "$@"
-
 rm -rf tmp build
+
