5a788f3b69a8e2a33869b54ae2ca2ab5b01c3d0f
Thijs Janssen <thijs@seecr.nl>
2013-05-17 13:49:59 +0200

    TJ: seecr-html testsetup Ok.

--- testsetup.sh
+++ testsetup.sh
@@ -46,6 +46,7 @@ cp -r test tmp/test
 removeDoNotDistribute tmp
 find tmp -name '*.py' -exec sed -r -e "
     s/\\\$Version:[^\\\$]*\\\$/\\\$Version: ${VERSION}\\\$/;
+    s,^docDir.*$,docDir = '$mydir/tmp/usr/share/doc/seecr-html',;
     " -i '{}' \;
 
 cp -r test tmp/test
