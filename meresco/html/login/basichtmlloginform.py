8b6c4c77ea18cb01403f8e51d940dda773446d1b
Thijs Janssen <thijs@seecr.nl>
2015-10-26 15:58:38 +0100

    HM/TJ: user can be None, a username or a user object.

--- meresco/html/login/basichtmlloginform.py
+++ meresco/html/login/basichtmlloginform.py
@@ -1,26 +1,27 @@
 ## begin license ##
 #
-# "Seecr Html" is a template engine based on generators, and a sequel to Slowfoot.
-# It is also known as "DynamicHtml".
+# "Meresco Html" is a template engine based on generators, and a sequel to Slowfoot.
+# It is also known as "DynamicHtml" or "Seecr Html".
 #
 # Copyright (C) 2012 Meertens Instituut (KNAW) http://meertens.knaw.nl
-# Copyright (C) 2012-2014 Seecr (Seek You Too B.V.) http://seecr.nl
+# Copyright (C) 2012-2015 Seecr (Seek You Too B.V.) http://seecr.nl
 # Copyright (C) 2014 Stichting Bibliotheek.nl (BNL) http://www.bibliotheek.nl
+# Copyright (C) 2015 Stichting Kennisnet http://www.kennisnet.nl
 #
-# This file is part of "Seecr Html"
+# This file is part of "Meresco Html"
 #
-# "Seecr Html" is free software; you can redistribute it and/or modify
+# "Meresco Html" is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
 #
-# "Seecr Html" is distributed in the hope that it will be useful,
+# "Meresco Html" is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with "Seecr Html"; if not, write to the Free Software
+# along with "Meresco Html"; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #
 ## end license ##
@@ -221,10 +222,12 @@ class BasicHtmlLoginForm(PostActions):
         formUrl = path
         if arguments:
             formUrl += "?" + urlencode(arguments, doseq=True)
+
+        username = session['user'].name if user is None else (user if isinstance(user, basestring) else user.name)
         values = dict(
             action=quoteattr(join(self._action, 'changepassword')),
             formUrl=quoteattr(formUrl),
-            username=quoteattr(session['user'].name if user is None else user),
+            username=quoteattr(username),
             lblOldPassword=getLabel(lang, "changepasswordForm", "old-password"),
             lblNewPassword=getLabel(lang, "changepasswordForm", "new-password"),
             lblNewPasswordRepeat=getLabel(lang, "changepasswordForm", "new-password-repeat"),
