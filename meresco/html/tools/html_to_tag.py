e3683be50cbcd694c311a69c0f3f090fcb533b24
Tom van der Sommen <tom@seecr.nl>
2018-03-13 14:22:36 +0100

    TS: license.

--- meresco/html/tools/html_to_tag.py
+++ meresco/html/tools/html_to_tag.py
@@ -1,3 +1,28 @@
+## begin license ##
+#
+# "Meresco Html" is a template engine based on generators, and a sequel to Slowfoot.
+# It is also known as "DynamicHtml" or "Seecr Html".
+#
+# Copyright (C) 2018 Seecr (Seek You Too B.V.) http://seecr.nl
+#
+# This file is part of "Meresco Html"
+#
+# "Meresco Html" is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# "Meresco Html" is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with "Meresco Html"; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#
+## end license ##
+
 import re
 from lxml.etree import Element, parse, XMLParser, QName
 from lxml.html.html5parser import document_fromstring, fragments_fromstring, fromstring, HTMLParser
